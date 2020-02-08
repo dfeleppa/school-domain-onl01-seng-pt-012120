d0a49a0382af872283663871dbf0caa33fa490cb
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-08 23:20:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,20 +3,12 @@ class School
   
   def initialize(school_name)
     @school_name = school_name
-    @roster = Hash.new{ |hash, key| hash[key] = [] }
+    @roster = Hash.new()
   end
   
   def add_student(student, grade)
     roster ||= []
     @roster[grade] << student
   end
-  
-  def grade(num)
-    num = 
-  
-  end
-  
-  def sort 
-    
-  end
+
 end
