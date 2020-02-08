1d2727dfad09cacd2aa0c0a454bc09b2983fa680
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-08 22:58:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,8 +3,8 @@ class School
   
   roster = {}
   
-  def add_student(student, age)
-    roster[student] << #{age}
-   end
+  def add_student(student, grade
+    roster[student] << #{grade}
+  end
 end
 
